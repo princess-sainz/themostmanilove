c631ea366999773ea942e971df7fa1924e3b4f68
princess-sainz <princessainzsss@gmail.com>
2026-06-06 19:48:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
-// 🎵 auto play fix
 window.addEventListener("click", function () {
   let music = document.getElementById("bgm");
   if (music) music.play();
 });
 
-// 🌠 shooting star
-function createStar() {
-  let star = document.createElement("div");
-  star.classList.add("star");
-
-  star.style.left = Math.random() * 100 + "vw";
-  star.style.animationDuration = (Math.random() * 2 + 2) + "s";
-
-  document.body.appendChild(star);
-
-  setTimeout(() => {
-    star.remove();
-  }, 4000);
-}
-
-setInterval(createStar, 300);
-
-// ⌨️ typing text
 let text = "selamat ulang tahun ya sayang 💙 semoga kamu selalu bahagia, sehat, dan jadi anak paling kuat dunia 🤍";
 let i = 0;
 
